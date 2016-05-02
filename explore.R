15f265e4584d8e1d4f1d82c94d60f4e7884bfd67
wjdddj <wj.dddj@gmail.com>
2016-05-02 00:55:06 -0700

    made a few more plots

--- explore.R
+++ explore.R
@@ -22,7 +22,8 @@ sf_train$PdDistrict  <- factor(sf_train$PdDistrict,
                              levels = names(sort(table(sf_train$PdDistrict), decreasing = TRUE)))
 sf_train$Resolution <- factor(sf_train$Resolution, 
                               levels = names(sort(table(sf_train$Resolution), decreasing = TRUE)))
-sf_train$DayOfWeek <- factor(sf_train$DayOfWeek)
+sf_train$DayOfWeek <- factor(sf_train$DayOfWeek,
+                             levels = c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"))
 
 sf_train$Day <- as.Date(sapply(sf_train$Dates, function(x)strsplit(x, ' ')[[1]][1]))
 
@@ -85,6 +86,17 @@ png('Category_byHour.png', 7*400, 6*300)
 grid_plot_ggplot2(plotByHour, nrow = 6, ncol = 7)
 dev.off()
 
+plotByDayOfWeek <- llply(1:length(sf_train_by_category), function(x){
+  ggplot(sf_train_by_category[[x]], aes(x = DayOfWeek)) + 
+    geom_bar() +
+    labs(x = 'Weekdays', y = 'count', title = names(sf_train_by_category)[[x]]) +
+    theme(title = element_text(size = 15), axis.title = element_text(size = 15), axis.text = element_text(size = 15),
+          axis.text.x = element_text(angle = 45, hjust = 1),
+          panel.grid = element_line(size = 1))
+})
+png('Category_byDayOfWeek.png', 7*400, 6*300)
+grid_plot_ggplot2(plotByDayOfWeek, nrow = 6, ncol = 7)
+dev.off()
 
 plotByPdDisrict <- llply(1:length(sf_train_by_category), function(x){
   ggplot(sf_train_by_category[[x]], aes(x = PdDistrict)) + 
@@ -98,6 +110,19 @@ png('Category_byPdDistrict.png', 7*400, 6*300)
 grid_plot_ggplot2(plotByPdDisrict, nrow = 6, ncol = 7)
 dev.off()
 
+plotByResolution <- llply(1:length(sf_train_by_category), function(x){
+  ggplot(sf_train_by_category[[x]], aes(x = Resolution)) + 
+    geom_bar() +
+    labs(x = 'Resolution', y = 'count', title = names(sf_train_by_category)[[x]]) +
+    theme(title = element_text(size = 15), axis.title = element_text(size = 15), axis.text = element_text(size = 15),
+          axis.text.x = element_text(angle = 45, hjust = 1),
+          panel.grid = element_line(size = 1))
+})
+png('Category_byResolution.png', 7*400, 6*600)
+grid_plot_ggplot2(plotByResolution, nrow = 6, ncol = 7)
+dev.off()
+
+
 x_scale = c(quantile(sf_train$X, 0.01), quantile(sf_train$X, 0.99))
 y_scale = c(quantile(sf_train$Y, 0.01), quantile(sf_train$Y, 0.99))
 geoPlotByPdDisrict <- llply(1:length(sf_train_by_category), function(x){
@@ -109,56 +134,16 @@ geoPlotByPdDisrict <- llply(1:length(sf_train_by_category), function(x){
           axis.text.x = element_text(angle = 45, hjust = 1),
           panel.grid = element_line(size = 1))
 })
-png('coords_ByPdDisrict.png', 7*1000, 6*800)
+png('coords_ByPdDisrict.png', 7*1200, 6*1000)
 grid_plot_ggplot2(geoPlotByPdDisrict, nrow = 6, ncol = 7)
 dev.off()
 
-png('coords_ByPdDisrict_1.png', 1000, 800, res = 200)
-ggplot(sf_train_by_category[[1]], aes(x = X, y = Y, color = PdDistrict)) + 
-  geom_point() +
-  coord_cartesian(xlim = x_scale, ylim = y_scale) + 
-  labs(x = 'longitude', y = 'latitute', title = names(sf_train_by_category)[[1]]) +
-  theme(title = element_text(size = 10), axis.title = element_text(size = 10), axis.text = element_text(size = 10),
-        axis.text.x = element_text(angle = 45, hjust = 1),
-        panel.grid = element_line(size = 1))
-dev.off()
 
 
 
 
-ggplot(sf_train, aes(x = Category, fill = PdDistrict)) + 
-  geom_bar(position = 'fill') + 
-  # scale_y_continuous(labels = percent) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-ggplot(sf_train, aes(x = PdDistrict, fill = Category)) + 
-  # geom_bar(position = 'fill') + 
-  geom_bar() + 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-ggplot(sf_train, aes(x = Category, fill = Resolution)) + 
-  geom_bar(position = 'fill') + 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-ggplot(sf_train, aes(x = Category, fill = DayOfWeek)) + 
-  geom_bar(position = 'fill') + 
-  # scale_y_continuous(labels = percent) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-
-png('coords_byPdDistrict.png', 2000, 2000, res = 200)
-plot(Y~X, data = sf_train[sample(878094, 10000, replace = F), ], pch = 16, cex = 0.3, col = factor(PdDistrict),
-     xlim = c(quantile(sf_train$X, 0.01), quantile(sf_train$X, 0.99)),
-     ylim = c(quantile(sf_train$Y, 0.01), quantile(sf_train$Y, 0.99)))
-dev.off()
-
-png('pd_cat.png', 2000, 2000, res = 200)
-plot(PdDistrict~Category, data = sf_train[sample(878094, 10000, replace = F), ], col = factor(Category))
-dev.off()
-
-sf_train$PdDistrict
-sf_train$Descript
-
+## some text mining
 install.packages('tm')
 install.packages('wordcloud')
 library(tm)
